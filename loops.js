69ca2be2e8377737a04933ba920b9b204ec1ed37
Sonny McPherson <unsungbomb@gmail.com>
2017-10-03 23:47:31 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,12 @@
+function (){
+  for (let i = 0; i < 25; i++) {
+  if i = 1 {
+    conslole.log("I am 1 strange loops
+    ")
+  }
+    else {
+      console.log(I am ${i} strange loops);
+    }
+  }
+  
+}
