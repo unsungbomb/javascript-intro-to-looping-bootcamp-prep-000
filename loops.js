b2464811acc37bba94c292a4a2fd024354607d62
Sonny McPherson <unsungbomb@gmail.com>
2017-10-03 23:56:33 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,11 +1,11 @@
 function forLoop(array) {
   for (let i = 0; i < 25; i++) {
-  if (i = 1) {
-    conslole.log("I am 1 strange loop")
+  if (i == 1) {
+    array.push("I am 1 strange loop")
   }
     else {
-      console.log("I am ${i} strange loops");
+      array.push("I am ${i} strange loops");
     }
   }
 
-}
+
